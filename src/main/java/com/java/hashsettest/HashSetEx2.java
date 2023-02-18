425bf7a8613b45869f270fbdb6ae07a68a7b4119
wooksun <uk1231@naver.com>
2023-02-18 21:51:57 +0900

    @Comparable과 HashSet에 대하여

---
+++ src/main/java/com/java/hashsettest/HashSetEx2.java
@@ -0,0 +1,24 @@
+package com.java.hashsettest;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class HashSetEx2 {
+
+	public static void main(String[] args) {
+		Set set = new HashSet();
+		
+		for(int i=0; set.size() < 6; i++) {
+			int num = (int)(Math.random()*45) +1;
+			set.add(new Integer(num));
+		}
+		
+		List list = new LinkedList(set);
+		Collections.sort(list);
+		System.out.println(list);
+	}
+
+}
