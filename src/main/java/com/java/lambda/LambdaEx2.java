7a0734e4352760a8b5ffc6ddb20fddbab85c3d9a
wooksun <uk1231@naver.com>
2023-03-28 18:35:57 +0900

    @외부 변수를 참조하는 람다식

--- src/main/java/com/java/lambda/LambdaEx2.java
+++ src/main/java/com/java/lambda/LambdaEx2.java
@@ -1,30 +1,30 @@
-package com.java.lambda;
-@FunctionalInterface
-interface MyFunction {
-	void myMethod(); //public abstract void myMethod();
-}
-
-public class LambdaEx2 {
-	public static void main(String[] args) {
-		MyFunction f = ()->{}; //MyFunction f = (MyFunction) ( ()->{});
-		Object obj = (MyFunction) ( ()->{}); //Object타입으로 형변환이 생략됨
-		String str = ((Object)(MyFunction) ( ()->{})).toString();
-		
-		System.out.println(f);
-		System.out.println(obj);
-		System.out.println(str);
-		
-//		System.out.println(()->{}); //에러. 람다식은 Object타입으로 형변환이 안됨
-		System.out.println((MyFunction) (()-> {}));
-		
-//		System.out.println((MyFunction)( ()-> {}).toString()); //에러
-		System.out.println(((Object)(MyFunction) ( ()-> {})).toString());
-	}
-
-}
-
-//일반적인 익명 객체라면,
-//객체의 타입이 '외부클래스이름$번호'와 같은 형식으로 타입이 결정
-
-//람다식의 타입은,
-//'외부클래스이름$$Lambda$번호'와 같은 형식으로 됨.
+//package com.java.lambda;
+//@FunctionalInterface
+//interface MyFunction {
+//	void myMethod(); //public abstract void myMethod();
+//}
+//
+//public class LambdaEx2 {
+//	public static void main(String[] args) {
+//		MyFunction f = ()->{}; //MyFunction f = (MyFunction) ( ()->{});
+//		Object obj = (MyFunction) ( ()->{}); //Object타입으로 형변환이 생략됨
+//		String str = ((Object)(MyFunction) ( ()->{})).toString();
+//		
+//		System.out.println(f);
+//		System.out.println(obj);
+//		System.out.println(str);
+//		
+////		System.out.println(()->{}); //에러. 람다식은 Object타입으로 형변환이 안됨
+//		System.out.println((MyFunction) (()-> {}));
+//		
+////		System.out.println((MyFunction)( ()-> {}).toString()); //에러
+//		System.out.println(((Object)(MyFunction) ( ()-> {})).toString());
+//	}
+//
+//}
+//
+////일반적인 익명 객체라면,
+////객체의 타입이 '외부클래스이름$번호'와 같은 형식으로 타입이 결정
+//
+////람다식의 타입은,
+////'외부클래스이름$$Lambda$번호'와 같은 형식으로 됨.
