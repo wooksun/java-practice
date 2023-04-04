1c158c3c44ef88c2afa4bef38d70bbfa2b8cb72d
wooksun <uk1231@naver.com>
2023-04-04 18:12:00 +0900

    @Stream<String[]>을 IntStream으러 변환하는 예제

--- src/main/java/com/java/stream/StreamEx5.java
+++ src/main/java/com/java/stream/StreamEx5.java
@@ -19,16 +19,17 @@ public class StreamEx5 {
 		System.out.println("sWor="+sWord.get());
 		
 		//Stream<String[]>을 IntStream으로 변환
+		//단어의 길이를 Int로 변환 -> Inheritance = 11, sum = 3 등등
 		IntStream intStream1 = Stream.of(strArr).mapToInt(String::length);
 		IntStream intStream2 = Stream.of(strArr).mapToInt(String::length);
 		IntStream intStream3 = Stream.of(strArr).mapToInt(String::length);
 		IntStream intStream4 = Stream.of(strArr).mapToInt(String::length);
 		
-		int count = intStream1.reduce(0, (a,b) -> a+1);
-		int sum = intStream2.reduce(0, (a,b) -> a+b);
+		int count = intStream1.reduce(0, (a,b) -> a+1); //단어의 수
+		int sum = intStream2.reduce(0, (a,b) -> a+b); //총 단어 길이의 합
 		
-		OptionalInt max = intStream3.reduce(Integer::max);
-		OptionalInt min = intStream4.reduce(Integer::min);
+		OptionalInt max = intStream3.reduce(Integer::max); //길이가 제일 긴 단어
+		OptionalInt min = intStream4.reduce(Integer::min); //길이가 제일 짧은 단어
 		
 		System.out.println("count="+count);
 		System.out.println("sum="+sum);
