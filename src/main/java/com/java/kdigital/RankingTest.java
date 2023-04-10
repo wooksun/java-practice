206a9b4eadb59569a43b0583a0f113ff8e78b4ee
wooksun <uk1231@naver.com>
2023-04-10 17:20:28 +0900

    @기본 석차 예제

---
+++ src/main/java/com/java/kdigital/RankingTest.java
@@ -0,0 +1,34 @@
+package k20230410;
+
+import java.util.Arrays;
+
+public class RankingTest {
+
+	public static void main(String[] args) {
+		
+		int[] score = {80, 100, 70, 100, 90};
+//		석차를 기억하는 기억장소는 1로 초기화 시킨다.
+		int[] rank = new int[score.length];  //위의 점수가 바뀌면, 자동으로 배열의 크기도 바뀜.
+		for(int i=0; i<rank.length; i++) {
+			rank[i] = 1;
+			
+		}
+//		System.out.println(Arrays.toString(rank));
+		
+		for(int i=0; i<score.length; i++) {
+			for(int j=0; j<score.length; j++) {
+//				내림차순 석차(큰 점수가 1등) , j번째가 크면 i번째가 1증가
+				if(score[i] < score[j]) {
+					rank[i]++;
+				}
+			}
+		}
+		
+		for(int i=0; i<score.length;i++) {
+			System.out.printf("%3d점은 %d등 입니다.\n" , score[i], rank[i]);
+		}
+		
+		
+	}
+
+}
